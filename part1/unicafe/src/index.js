2efc3cb0e86c5209515653bbc821459eca569907
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-20 12:39:56 +0100

    1.10: renamed component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 const Title = ({ text }) => <h1>{text}</h1>
 
-const FeedbackStats = ({ text, number }) => {
+const Statistic = ({ text, number }) => {
     return (
         <p>{text} {number} {text == 'positive' ? '%' : ''}</p>
     )
@@ -22,12 +22,12 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
         <>
             <Title text='statistics' />
-            <FeedbackStats text='good' number={good} />
-            <FeedbackStats text='neutral' number={neutral} />
-            <FeedbackStats text='bad' number={bad} />
-            <FeedbackStats text='all' number={good + neutral + bad} />
-            <FeedbackStats text='average' number={(good - bad) / (good + neutral + bad)} />
-            <FeedbackStats text='positive' number={good / (good + neutral + bad) * 100} />
+            <Statistic text='good' number={good} />
+            <Statistic text='neutral' number={neutral} />
+            <Statistic text='bad' number={bad} />
+            <Statistic text='all' number={good + neutral + bad} />
+            <Statistic text='average' number={(good - bad) / (good + neutral + bad)} />
+            <Statistic text='positive' number={good / (good + neutral + bad) * 100} />
         </>
     )
 }
@@ -45,9 +45,7 @@ const App = () => {
     const [bad, setBad] = useState(0)
 
     const handleGoodClick = () => setGood(good + 1)
-
     const handleNeutralClick = () => setNeutral(neutral + 1)
-
     const handleBadClick = () => setBad(bad + 1)
 
     return (
