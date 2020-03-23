99b7ec5c0e1fb0843fec3f1ce7ad44b2c38c0ad2
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-23 14:07:03 +0100

    2.9: added case-insensitive filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '040-1234567'
-        }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
+
+    const numberToShow = filter === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(filter))
 
     const addNumber = (event) => {
         event.preventDefault()
@@ -34,9 +37,17 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input value={filter} onChange={handleFilterChange} />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addNumber}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
@@ -50,7 +61,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+                {numberToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
             </div>
         </div>
     )
