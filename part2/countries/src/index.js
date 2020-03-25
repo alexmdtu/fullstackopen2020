26c08f9df6d2db9d4b8cb73c56826797a234f52b
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-25 15:43:08 +0100

    2.14: added weather data

--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -53,6 +53,17 @@ const CountryAndButton = ({ country }) => {
 }
 
 const Country = ({ country }) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
+      .then(response => {
+        setWeather(response.data.current)
+      })
+  }, [api_key, country.capital])
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -62,7 +73,12 @@ const Country = ({ country }) => {
       <ul>
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
-      <img src={country.flag} alt={country.name} width="200px" />
+      <img src={country.flag} alt={country.name} width='200px' />
+      <h3>Weather in {country.capital}</h3>
+      <h4>temperature: {weather.temperature} ˚C</h4>
+      {/* {console.log(weather.weather_icons ? weather.weather_icons[0] : 'init not done')} */}
+      <img src={weather.weather_icons ? weather.weather_icons[0] : ''} alt='a weather icon' width='100px' />
+      <h4>wind: {weather.wind_speed} mph direction {weather.wind_dir}</h4>
     </div>
 
   )
