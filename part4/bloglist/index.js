f0ba028e14521b77e9e9d5f54182329eaba5d1e2
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-06 11:57:10 +0200

    4.2: refactored into separate app file

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,21 +1,10 @@
+const app = require('./app')
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
