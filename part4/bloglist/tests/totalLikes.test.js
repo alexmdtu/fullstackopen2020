d95a063fe677918b596537889d929548f9817761
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-06 13:09:50 +0200

    4.4: added unit tests for totalLikes

---
+++ part4/bloglist/tests/totalLikes.test.js
@@ -0,0 +1,54 @@
+const listHelper = require('../utils/list_helper')
+
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithMoreBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f3',
+            title: 'Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 10,
+            __v: 0
+        }
+    ]
+
+    test('of empty list is zero', () => {
+        expect(listHelper.totalLikes([])).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        expect(listHelper.totalLikes(listWithMoreBlogs)).toBe(25)
+    })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, currentBlog) => {
+        return acc + currentBlog.likes
+    }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
