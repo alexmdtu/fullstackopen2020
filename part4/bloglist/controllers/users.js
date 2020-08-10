b516be2a7002bfb1d6c1998505f934340f97cc14
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-10 13:56:52 +0200

    4.15: Implemented user addition with hashed password.

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash: passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+    const user = await User.find({})
+
+    response.json(user)
+})
+
+module.exports = usersRouter
