87a89fa62b9c02568c92762610caa2b3cd0ad9d1
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-19 15:16:35 +0200

    6.8: refactored displaying of anecdotes and voting into separate component.

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = (props) => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    return (
+        anecdotes
+            .sort((a, b) => a.votes - b.votes)
+            .map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+                    </div>
+                </div>
+            )
+    )
+}
+
+export default AnecdoteList
