90b934ad313701837f1c1530c217725dfa860551
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-26 16:11:12 +0100

    8.4 + 8.5: added optional arguments to allBooks query.

--- part8/graphql-exercise/index.js
+++ part8/graphql-exercise/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(genre: String, author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,19 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.genre && args.author) {
+        return books.filter(book => book.genres.includes(args.genre) && book.author === args.author)
+      }
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
+      if (args.author) {
+        return books.filter(book => book.author === args.genre)
+      }
+
+      return books
+    },
     allAuthors: () => authors
   },
   Author: {
