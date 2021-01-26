7dc74b46308f1eb5bedd9b513e90eaabde72a1b9
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-26 22:17:14 +0100

    8.6: Implemented addBook mutation.

--- part8/graphql-exercise/index.js
+++ part8/graphql-exercise/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks(genre: String, author: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -128,6 +138,23 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter(book => book.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      // save author if not present
+      if (!authors.find(n => n.name === args.author)) {
+        const author = {
+          name: args.author,
+          id: uuid(),
+          born: null
+        }
+        authors.concat(author)
+      }
+      // save book
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
