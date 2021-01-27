f067d595cb830a51b34865338a10f497616bc27b
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-27 12:16:27 +0100

    Added ability to change phone number.

--- part8/apollo-client-example/src/App.js
+++ part8/apollo-client-example/src/App.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
+import PhoneForm from './components/PhoneForm';
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -26,6 +27,7 @@ const App = () => {
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
+      <PhoneForm />
     </div>
   )
 }
