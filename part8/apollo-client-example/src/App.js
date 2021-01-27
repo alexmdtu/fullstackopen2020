b11c21d5142f03002c0a0d558521de107c88a528
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-27 12:13:10 +0100

    Added error handling.

--- part8/apollo-client-example/src/App.js
+++ part8/apollo-client-example/src/App.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client';
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm all_persons={ALL_PERSONS} />
+      <PersonForm setError={notify} />
+    </div>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return (
+    <div style={{ color: 'red' }}>
+      {errorMessage}
     </div>
   )
 }
