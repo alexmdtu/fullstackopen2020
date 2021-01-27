0ca8c01742a58aaeb000fe4b785e609a8091743b
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-27 13:56:32 +0100

    8.10: Implemented adding book functionality.

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -20,3 +20,19 @@ export const ALL_BOOKS = gql`
     }
   }
 `
+
+export const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $publishedInt: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $publishedInt,
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
