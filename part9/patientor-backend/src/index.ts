987b373639822e35f58a589743397147bf6a52d4
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-31 22:12:22 +0100

    9.10: Created diagnose get endpoint.

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
+import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
-
-import cors from 'cors';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 
@@ -12,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
