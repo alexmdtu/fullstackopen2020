d2fdf02b0a15d99827df6fdc684512f58e72eb36
Alexander Tu <alexander.md.tu@gmail.com>
2021-01-31 22:25:14 +0100

    9.11: Created patient get endpoint hiding ssn field.

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,21 @@
+import patients from '../data/patients';
+import { Patient, NonSensitivePatientEntry } from '../types';
+
+const getPatientEntries = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatientEntries = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+};
+
+export default {
+  getPatientEntries,
+  getNonSensitivePatientEntries
+};
